ecdb7305bc4bf5c4ce99b839abfbf96998a59278
FaezehKhoshayand <faezehkhoshayand@gmail.com>
2024-03-21 18:35:35 +0330

    attributes and constructor

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -2,35 +2,40 @@ import java.util.ArrayList;
 public class Hogwarts {
 
     // TODO: Define Attributes
-    static ArrayList<String> studentsList = new ArrayList<>();
-    static ArrayList<String> teachersList = new ArrayList<>();
-    static ArrayList<String> coursesList = new ArrayList<>();
+    static ArrayList<Student> studentsList;
+    static ArrayList<Teacher> teachersList;
+    static ArrayList<Course> coursesList;
 
     // TODO: Define Functionalities
-    public void viewAllTeachers() {
+    Hogwarts() {
+        studentsList = new ArrayList<>();
+        teachersList = new ArrayList<>();
+        coursesList = new ArrayList<>();
+    }
+    static public void viewAllTeachers() {
         //TODO
         int i = 1;
-        for(String teacher : teachersList) {
+        for(Teacher teacher : teachersList) {
             System.out.print(i + "-" + teacher);
             i++;
         }
         System.out.println();
     }
 
-    public void viewAllStudents() {
+    static public void viewAllStudents() {
         //TODO
         int i = 1;
-        for(String student : studentsList) {
+        for(Student student : studentsList) {
             System.out.print(i + "-" + student);
             i++;
         }
         System.out.println();
     }
 
-    public void viewAllCourses() {
+    static public void viewAllCourses() {
         //TODO
         int i = 1;
-        for(String course : coursesList) {
+        for(Course course : coursesList) {
             System.out.print(i + "-" + course);
             i++;
         }
