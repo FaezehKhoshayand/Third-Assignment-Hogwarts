7e8fb26d9d2153caf05bb7eee7e28ecfa1089968
FaezehKhoshayand <faezehkhoshayand@gmail.com>
2024-03-22 16:06:19 +0330

    check if an assistant has logged in

--- src/main/java/Assistant.java
+++ src/main/java/Assistant.java
@@ -2,44 +2,81 @@ import java.util.ArrayList;
 
 public class Assistant extends Account{
 
+    boolean hasLogged;
     Assistant(String username, String password) {
         super(username,password);
     }
     public void removeTeacher(Teacher teacher) {
-        Hogwarts.teachersList.remove(teacher);
+        if(hasLogged) {
+            Hogwarts.teachersList.remove(teacher);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
     public void removeStudent(Student student) {
-        Hogwarts.studentsList.remove(student);
+        if(hasLogged) {
+            Hogwarts.studentsList.remove(student);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
     public void viewCourseStudent() {
-        int i = 1;
-        for(Course course : Hogwarts.coursesList) {
-            System.out.println(i + "-Title: " + course.courseTitle + " , Students: " + course.courseStudents);
-            i++;
+        if(hasLogged) {
+            int i = 1;
+            for(Course course : Hogwarts.coursesList) {
+                System.out.println(i + "-Title: " + course.courseTitle + " , Students: " + course.courseStudents);
+                i++;
+            }
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
         }
     }
     public void checkProfileStudent(Student student) {
-        System.out.println("Student's User Name: " + student.getUsername() + " Password: " + student.getPassword() + "House: " + student.house + "Courses: " + student.courses);
+        if(hasLogged) {
+            System.out.println("Student's User Name: " + student.getUsername() + " Password: " + student.getPassword() + "House: " + student.house + "Courses: " + student.courses);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
 
     public void checkProfileTeacher(Teacher teacher) {
-        System.out.println("Student's User Name: " + teacher.getUsername() + " Password: " + teacher.getPassword() + "Courses: " + teacher.courses);
+        if(hasLogged) {
+            System.out.println("Student's User Name: " + teacher.getUsername() + " Password: " + teacher.getPassword() + "Courses: " + teacher.courses);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
 
     public void createCourse(Course course) {
-        Hogwarts.addCourse(course);
+        if(hasLogged) {
+            Hogwarts.addCourse(course);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
     //signup
     public void signup(Assistant assistant) {
+        if(hasLogged) {
             Hogwarts.assistantsList.add(assistant);
+        }
+        else {
+            System.out.println("YOU ARE NOT ALLOWED TO DO THIS.");
+        }
     }
-    public boolean login(String username,String password) {
+    public Assistant login(String username,String password) {
         for(Assistant assistant : Hogwarts.assistantsList) {
             if(assistant.getUsername().equals(username) && assistant.getPassword().equals(password)) {
-                return true;
+                hasLogged = true;
+                return assistant;
             }
         }
-        return false;
+        System.out.println("Invalid!");
+        return null;
     }
-    //security check if they have logged in
 }
