7a13fb79e8d0471599e06df30c8f5fbe065fb946
FaezehKhoshayand <faezehkhoshayand@gmail.com>
2024-03-22 19:07:33 +0330

    a better version of takeCourse

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Teacher extends Account{
+    double score;
     ArrayList<Course> courses;
     public Teacher(String username, String password) {
         super(username,password);
         courses = new ArrayList<>();
+        score = 0;
     }
-    //access
-    //sign up
-    //login
     public void viewCourses() {
         int i = 1;
         for(Course course : courses) {
@@ -17,23 +16,35 @@ public class Teacher extends Account{
                 i++;
         }
     }
-    public void takeCourse() {//each course has a teacher so how would a teacher wanna take a course--__----
-        System.out.println("Which one do you want to take?");
-        Hogwarts.viewAllCourses();
+    public void takeCourse() {
+        int i = 1;
+        for(Course course : Hogwarts.requestedCourses ) {
+            System.out.println(i + "-" + course.courseTitle);
+            i++;
+        }
+        System.out.println("Enter the number of the course you're looking for");
         Scanner in = new Scanner(System.in);
-        in.nextInt();
-        Course newCourse = new Course();
-        Hogwarts.addCourse(newCourse);
+        int index = in.nextInt() - 1;
+        Hogwarts.requestedCourses.get(index).courseTeacher = this;
     }
+
     public void signUp(Teacher teacher) {
         ////
         Hogwarts.teachersList.add(teacher);
     }
-    public boolean login(String username,String password) {
+    public Teacher login(String username,String password) {
         for(Teacher teacher : Hogwarts.teachersList) {
             if(teacher.getUsername().equals(username) && teacher.getPassword().equals(password)) {
-                return true;
+                return teacher;
             }
         }
-        return false;
-    }}
+        System.out.println("Invalid!");
+        return null;
+    }
+    public void studentScoring(Student student,double score) {
+        student.setScore(score);
+    }
+    public void requestCourse(Course course) {
+        Hogwarts.addRequestedCourse(course);
+    }
+}
